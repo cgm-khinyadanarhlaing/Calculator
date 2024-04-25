31519b54dc4c4d0b2988d17d9fceacb2aad86fe1
Khin Yadanar Hlaing <khinyadanarhlaing@cgm-myanmar.com>
2024-04-25 16:27:40 +0630

    Calculator App

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,18 +5,23 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var strNumber: String = ""
+    private var strNumber2:String = ""
+    private var number1:Double = 0.0
+    private var number2:Double = 0.0
+    private var result:Double = 0.0
+    private var operator:Boolean = false
+    private var operatorSign:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        var num1:Double
-        var num2:Double
-        var operator:String
-        val tvDisplay: TextView = findViewById(R.id.tvDisplay)
+        val tvDisplay1: TextView = findViewById(R.id.tvDisplay1)
 
         val button0:Button = findViewById(R.id.button0)
         val button1:Button = findViewById(R.id.button1)
@@ -38,44 +43,218 @@ class MainActivity : AppCompatActivity() {
         val eqlBtn:Button = findViewById(R.id.eqlBtn)
 
         clrBtn.setOnClickListener {
-            tvDisplay.clearComposingText();
-            tvDisplay.text = "0"
+            tvDisplay1.clearComposingText();
+            tvDisplay1.text = "0"
+            strNumber = ""
+            strNumber2 = ""
+            operatorSign =""
+        }
+        delBtn.setOnClickListener {
+            operator = lastElementOperator(strNumber)
+            if (operator) {
+                operatorSign = ""
+            }
+            strNumber = delete (strNumber)
+            tvDisplay1.text = strNumber
+        }
+        addBtn.setOnClickListener {
+            if (strNumber.isNotEmpty() && strNumber.isDigitsOnly()) {
+                number1 = strNumber.toDouble()
+                strNumber += "+"
+            }
+            operatorSign = "+";
+            tvDisplay1.text = strNumber
+            strNumber2 = ""
+        }
+        subBtn.setOnClickListener {
+            if (strNumber.isNotEmpty() && strNumber.isDigitsOnly()) {
+                number1 = strNumber.toDouble()
+                strNumber += "-"
+            }
+            operatorSign = "-";
+            tvDisplay1.text = strNumber
+            strNumber2 = ""
+        }
+        mulBtn.setOnClickListener {
+            if (strNumber.isNotEmpty() && strNumber.isDigitsOnly()) {
+                number1 = strNumber.toDouble()
+                strNumber += "*"
+            }
+            operatorSign = "*";
+            tvDisplay1.text = strNumber
+            strNumber2 = ""
+        }
+        divBtn.setOnClickListener {
+            if (strNumber.isNotEmpty() && strNumber.isDigitsOnly()) {
+                number1 = strNumber.toDouble()
+                strNumber += "/"
+            }
+            operatorSign = "/";
+            tvDisplay1.text = strNumber
+            strNumber2 = ""
+        }
+        resultBtn.setOnClickListener {
+            if (strNumber.isNotEmpty() && strNumber.isDigitsOnly()) {
+                number1 = strNumber.toDouble()
+                strNumber += "%"
+            }
+            operatorSign = "%";
+            tvDisplay1.text = strNumber
+            strNumber2 = ""
+        }
+        eqlBtn.setOnClickListener {
+            if (strNumber2.isNotEmpty() && strNumber2.isDigitsOnly()) {
+                number2 = strNumber2.toDouble();
+                if (operatorSign == "+") {
+                    result = addition(number1, number2)
+                    tvDisplay1.text = result.toString()
+                } else if (operatorSign == "-") {
+                    result = subtraction(number1, number2)
+                    tvDisplay1.text = result.toString()
+                } else if (operatorSign == "/") {
+                    result = division(number1, number2)
+                    tvDisplay1.text = result.toString()
+                } else if (operatorSign == "*") {
+                    result = multiply(number1, number2)
+                    tvDisplay1.text = result.toString()
+                } else {
+                    result = resultByDiv(number1, number2)
+                    tvDisplay1.text = result.toString()
+                }
+            }
+            strNumber = ""
+            strNumber2 = ""
+            operatorSign = ""
         }
         button0.setOnClickListener {
-            tvDisplay.text = "0"
+            strNumber += "0"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "0"
+            }
         }
         button1.setOnClickListener {
-            tvDisplay.text = "1"
+            strNumber += "1"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "1"
+            }
         }
         button2.setOnClickListener {
-            tvDisplay.text = "2"
+            strNumber += "2"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "2"
+            }
         }
         button3.setOnClickListener {
-            tvDisplay.text = "3"
+            strNumber += "3"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "3"
+            }
         }
         button4.setOnClickListener {
-            tvDisplay.text = "4"
+            strNumber += "4"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "4"
+            }
         }
         button5.setOnClickListener {
-            tvDisplay.text = "5"
+            strNumber += "5"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "5"
+            }
         }
         button6.setOnClickListener {
-            tvDisplay.text = "6"
+            strNumber += "6"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "6"
+            }
         }
         button7.setOnClickListener {
-            tvDisplay.text = "7"
+            strNumber += "7"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "7"
+            }
         }
         button8.setOnClickListener {
-            tvDisplay.text = "8"
+            strNumber += "8"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "8"
+            }
         }
         button9.setOnClickListener {
-            tvDisplay.text = "9"
+            strNumber += "9"
+            tvDisplay1.text = strNumber
+            operator = lastElementOperator(strNumber)
+            if (operator || operatorSign.isNotEmpty()) {
+                strNumber2 += "9"
+            }
         }
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun delete(inputString: String): String {
+        return if (inputString.isNotEmpty()) {
+            inputString.dropLast(1)
+        } else {
+            inputString
+        }
+    }
+
+    fun lastElementOperator(inputString:String): Boolean {
+        if (inputString.isNotEmpty()) {
+            val lastElement = inputString.last()
+            return when (lastElement) {
+                '+', '-', '*', '%', '/' -> true
+                else -> false
+            }
+        } else {
+            return false
+        }
+    }
+    fun addition(num1:Double , num2:Double) : Double {
+        val answer:Double = num1 + num2;
+        return answer;
+    }
+
+    fun subtraction(num1:Double , num2:Double) : Double {
+        val answer:Double = num1 - num2;
+        return answer;
+    }
+
+    fun division(num1:Double , num2:Double) : Double {
+        val answer:Double = num1 / num2;
+        return answer;
+    }
+    fun multiply(num1:Double , num2:Double) : Double {
+        val answer:Double = num1 * num2;
+        return answer;
+    }
+
+    fun resultByDiv(num1:Double , num2:Double) : Double {
+        val answer:Double = num1 % num2;
+        return answer;
     }
 }
